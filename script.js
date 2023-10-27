eb92e660b52a9c9364f8c4c2e2c3809a8c22649b
Michal Jurczuk <michal.jurczuk91@gmail.com>
2023-10-27 17:32:02 +0200

    basic framework

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Wykorzystując API Ricky i Morty, napisz prostą stronę składającą się z
+dwóch podstron. Na pierwszej głównej stronie wyświetl postacie i zastosuj paginację. Stwórz też na tej stronie.
+search który będzie filtrował te postacie po nazwie i po zatwierdzeniu Enterem pokazywał
+nową listę postaci.
+Każdy kafelek z postacią powinien zawierać dane takie jak: name, image, species.
+W przypadku braku grafiki wyświetlić dowolny placeholder.
+Druga strona będzie uruchamiana po kliku w kafelek z postacią, ma przedstawiać
+bardziej szczegółowe dane postaci wraz z listą odcinków w jakich brała udział (lista ma zawierać numer oraz nazwę odcinka).
+ Dodać dane do postaci takie jak: gender, location name, status oraz listę odcinków. Druga strona powinna zawierać strzałkę powrotu do pierwszej strony.
+Do warstwy wizualnej dowolne wykorzystanie sposobu stylowania, mile będzie widziane wykorzystanie metodologii BEM i zadbanie o czytelność strony. Ważne aby strona była RWD.
+Link do dokumentacji API:
+graphql: https://rickandmortyapi.com/documentation/#graphql
+REST: https://rickandmortyapi.com/documentation/#rest
+ */
+
+
+async function getCharacters(search = "") {
+    let characters = null;
+    if (!search) {
+        let response = await fetch("https://rickandmortyapi.com/api/character");
+        characters = await response.json();
+    }
+    else {
+        let response = await (fetch("https://rickandmortyapi.com/api/character/?name=" + search));
+        characters = await response.json();
+        console.log("else");
+    }
+    console.log(characters);
+    return characters;
+}
